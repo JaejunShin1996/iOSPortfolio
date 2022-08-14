4f9ac03ac58203583561131dbc70ecbea93203ba
JaejunShin1996 <jaeshinkorean@gmail.com>
2022-08-14 18:31:35 +0930

    Editing values

---
+++ JaejunShinPortfolio/Binding-OnChange.swift
@@ -0,0 +1,8 @@
+//
+//  Binding-OnChange.swift
+//  JaejunShinPortfolio
+//
+//  Created by Jaejun Shin on 14/8/2022.
+//
+
+import Foundation
---
+++ JaejunShinPortfolio/ItemRowView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemRowView.swift
+//  JaejunShinPortfolio
+//
+//  Created by Jaejun Shin on 14/8/2022.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView()
+    }
+}
