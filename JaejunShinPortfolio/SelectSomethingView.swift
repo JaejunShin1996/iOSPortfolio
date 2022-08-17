55cd1584d55d5696dfca77e418af79159931f14e
JaejunShin1996 <jaeshinkorean@gmail.com>
2022-08-17 16:17:12 +0930

    Fixed some bugs on deleting and added some UI on ProjectView

---
+++ JaejunShinPortfolio/SelectSomethingView.swift
@@ -0,0 +1,22 @@
+//
+//  SelectSomethingView.swift
+//  JaejunShinPortfolio
+//
+//  Created by Jaejun Shin on 17/8/2022.
+//
+
+import SwiftUI
+
+struct SelectSomethingView: View {
+    var body: some View {
+        Text("Please select somthing to begin")
+            .italic()
+            .foregroundColor(.secondary)
+    }
+}
+
+struct SelectSomethingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectSomethingView()
+    }
+}
